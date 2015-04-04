8f17d3b2ca287e60e51fc2b175dbf287c6797581
anton-johansson <antoon.johansson@gmail.com>
2015-04-04 12:12:40 +0200

    Initial code base

---
+++ README.md
@@ -0,0 +1,4 @@
+log4js-logstash-appender
+========================
+
+A simple log4js appender that sends a JSON-object to a Logstash server.
---
+++ index.js
@@ -0,0 +1,65 @@
+"use strict"
+
+var dgram = require('dgram');
+var util = require('util');
+var storage = require('continuation-local-storage');
+var MDC = storage.createNamespace('log4js-MDC');
+
+/** Sends data to logstash. */
+function send(socket, host, port, loggingObject)
+{
+	var buffer = new Buffer(JSON.stringify(loggingObject));
+	socket.send(buffer, 0, buffer.length, port, host, function(err, bytes)
+	{
+		if (err)
+		{
+			console.error('log4js-logstash-appender (%s:%p) - %s', host, port, util.inspect(err));
+		}
+	});
+}
+
+/** Returns the appender. */
+function appender(configuration)
+{
+	var socket = dgram.createSocket('udp4');
+
+	return function(loggingEvent)
+	{
+		var loggingObject =
+		{
+			name: loggingEvent.categoryName,
+			message: loggingEvent.data[0],
+			severity: loggingEvent.level.level,
+			severityText: loggingEvent.level.levelStr,
+		};
+
+		if (configuration.application)
+		{
+			loggingObject.application = configuration.application;
+		}
+
+		if (configuration.environment)
+		{
+			loggingObject.environment = configuration.environment;
+		}
+
+		if (MDC && MDC.active)
+		{
+			Object.keys(MDC.active).forEach(function(key)
+			{
+				loggingObject[key] = MDC.active[key];
+			});
+		}
+
+		send(socket, configuration.host, configuration.port, loggingObject);
+	};
+}
+
+/** Configures the logstash appender. */
+function configure(configuration)
+{
+	return appender(configuration);
+};
+
+exports.appender = appender;
+exports.configure = configure;
